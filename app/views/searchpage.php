45696019b898eaa599c0fcb1f92c29d340a03bc3
DieaCalin <86836705+DieaCalin@users.noreply.github.com>
2022-11-17 13:34:12 +0200

    not working

--- app/views/searchpage.php
+++ app/views/searchpage.php
@@ -1,4 +1,5 @@
 <head>
+<?php include('templates/navbar.php'); ?>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
   <script src="../../node_modules/lodash/lodash.js"></script>
   <!-- C:\MAMP\htdocs\MVC\node_modules\lodash\lodash.js -->
@@ -25,8 +26,35 @@
           <th scope="col">Actions</th>
         </tr>
       </thead>
-      <tbody id="contentTable">
-
+      <tbody id="contentTable"> <?php
+if($data) {
+foreach ($data as $value) {
+  ?>
+      <tr id="movie-<?php echo $value['MovieID']; ?>">
+        <td>
+          <?php print_r($value['MovieTitle']) ?>
+        </td>
+        <td>
+          <?php print_r($value['DirectorName']) ?>
+        </td>
+        <td>
+          <?php print_r($value['GenreName']) ?>
+        </td>
+        <td>
+          <?php print_r($value['MovieRating']) ?>
+        </td>
+        <td>
+          <?php print_r($value['MovieDescription']) ?>
+        </td>
+        <td><a class="btn btn-primary" href="<?php echo PUBLIC_PATH ?>movie/?id=<?php echo $value['MovieID'] ?>"> Show
+          </a></td>
+        <td><button onclick="deleteItem(<?php echo $value['MovieID']; ?>)" type="submit"
+            value="<?php echo $value['MovieID']; ?>" id="delete" class="btn btn-danger">Delete</button></td>
+      </tr>
+      <?php
+  }
+}
+  ?>
     </tbody>
     </table>
 </div>
@@ -48,7 +76,6 @@ function render(response){
     "</tr>"
     );
   }
-
 }
 
 function debounce( callback, delay ) {
@@ -68,7 +95,17 @@ myInput.addEventListener(
 
     function searchCache() {
       if(myInput.value) {
+        const url = new URL(window.location.href);
+        url.searchParams.set('q', myInput.value);
+
+        window.history.replaceState(null, null, url); // or pushState
         search(myInput.value);
+      } else {
+        const url = new URL(window.location.href);
+        var currentUrl = url.HREF;
+        console.log(currentUrl);
+        let params = new URLSearchParams(currentUrl);
+        params.delete('q'); //Query string is now: 'bar=2'
       }
     }
 
