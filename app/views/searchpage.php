b91dbf8a6f55d4049b82adf0d8b49a8d97ac4f65
DieaCalin <86836705+DieaCalin@users.noreply.github.com>
2022-11-17 12:13:36 +0200

    working search with debouncer

--- app/views/searchpage.php
+++ app/views/searchpage.php
@@ -1,5 +1,7 @@
 <head>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script src="../../node_modules/lodash/lodash.js"></script>
+  <!-- C:\MAMP\htdocs\MVC\node_modules\lodash\lodash.js -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
     integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
@@ -15,10 +17,28 @@
 </div>
 
 <script>
-    $('#searchbar').on("keyup change",function(e){
-        console.log(this.value);
-        search(this.value);
-    });
+
+function debounce( callback, delay ) {
+    let timeout;
+    return function() {
+        clearTimeout( timeout );
+        timeout = setTimeout( callback, delay );
+    }
+}
+
+const myInput = document.getElementById("searchbar");
+
+myInput.addEventListener(
+    "keyup",
+    debounce( searchCache, 1000 )
+);
+
+    function searchCache() {
+      if(myInput.value) {
+        search(myInput.value);
+      }
+    }
+
 
     function search(value) {
     //alert (javascriptVariable);
@@ -29,7 +49,7 @@
       data: value,
       success: function(response) {
        console.log(response);
-            },
+      },
     });
   }
 
